f955ff051a7958d65c5728fdb12697c9642ea192
Amrit Mishra <myselfamrit1596mishra@gmail.com>
2026-05-15 17:00:33 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerUrl  = https://interviewiq-backend-ixeg.onrender.com
+export const ServerUrl  = "https://interviewiq-backend-ixeg.onrender.com"
 
 function App() {
 
